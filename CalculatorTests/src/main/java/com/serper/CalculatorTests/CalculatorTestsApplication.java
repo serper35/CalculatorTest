3945841903aecdc17ea20e4d3efd75faad7a301e
serper35 <serper.wolf@gmail.com>
2023-12-13 19:26:57 +0300

    test

---
+++ CalculatorTests/src/main/java/com/serper/CalculatorTests/CalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package com.serper.CalculatorTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestsApplication.class, args);
+	}
+
+}
